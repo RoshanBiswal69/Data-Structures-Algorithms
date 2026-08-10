20c781beb6efdb9a5d36623914deeacca1af9998
Roshan Biswal <roshanbiswal9439@gmail.com>
2026-08-11 01:05:59 +0530

    Time: 3 ms (11.68%), Space: 9.3 MB (26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> count(26,0);
-        int n = s.size();
-        for(int i =0;i<n;i++){
+        std::vector<int> count(26,0);
+        for(int i=0;i<s.length();i++){
             count[s[i]-'a']++;
         }
-        for(int i =0;i<t.size();i++){
+        for(int i=0;i<t.length();i++){
             count[t[i]-'a']--;
-            if(count[t[i]-'a'] == -1){
+            if(count[t[i]-'a']==-1){
                 return t[i];
             }
         }
