f5434174c5865ba198aad436a5068b01c2bfeb76
Roshan Biswal <roshanbiswal9439@gmail.com>
2026-09-20 22:39:42 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
-    int bound(vector<int>& nums,int target,bool upper){
-        int left = 0;
-        int right = nums.size()-1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            
-             if (nums[mid] < target || upper && nums[mid] == target) left = mid + 1;
-            else right = mid -1;
-        }
-        return left;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = bound(nums, target, false);
-        if(first == nums.size() || nums[first]!= target) return {-1,-1};
-        int last = bound(nums,target,true) -1;
-        return {first,last};
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return {first, last};
+    }
+
+private:
+    int findFirst(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    int findLast(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return last;
     }
 };
